bce5a2ba786c83dbe4e17c68be3f2370b53da0a1
Kim Robinson <kimmykokonut@gmail.com>
2024-03-08 16:04:11 -0800

    pass test to insert node and remove node head by index

--- __tests__/linked-list.test.js
+++ __tests__/linked-list.test.js
@@ -1,9 +1,40 @@
 import LinkedList from "../src/linked-list";
 
 describe('LinkedList', () => {
+
+  let linkedListWithNodes = new LinkedList();
+
+  beforeEach(() => {
+    linkedListWithNodes.insertLast("node1");
+    linkedListWithNodes.insertLast("node2");
+    linkedListWithNodes.insertLast("node3");
+    linkedListWithNodes.insertLast("node4");
+  });
+  //cleanup-like deleting the head so tests don't pollute each other
+  afterEach(() => {
+    linkedListWithNodes.head = null;
+  });
+
   test('should construct a linked list with a head property', () => {
     let linkedList = new LinkedList();
     expect(linkedList.head).toEqual(null);
   });
-
+  test('LinkedList.prototype.insertLast() should add a node at head if list has no head', () => {
+    let linkedList = new LinkedList();
+    linkedList.insertLast("head");
+    expect(linkedList.head.data).toEqual("head");
+  });
+  test('LinkedList.prototype.insertLast() should add a node at the end of a linked list', () => {
+    linkedListWithNodes.insertLast("new last");
+    expect(linkedListWithNodes.head.next.next.next.next.data).toEqual("new last");
+  });
+  test('it should set a new head if the head is removed', () => {
+    linkedListWithNodes.remove(0);
+    expect(linkedListWithNodes.head.data).toEqual("node2");
+  });
+  test('it should remove a node from the middle', () => {
+    linkedListWithNodes.remove(2);
+    expect(linkedListWithNodes.head.next.next.data).toEqual("node4");
+  });
+  
 });
--- src/linked-list.js
+++ src/linked-list.js
@@ -1,6 +1,22 @@
+import Node from "./node";
+
 export default class LinkedList {
   constructor() {
     this.head = null;
   }
-  
+  insertLast(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next != null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
+  remove(indexToRemove) {
+    this.head = this.head.next;
+  }
 }
